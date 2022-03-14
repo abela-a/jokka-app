3932ae4b56653fb87b23d81a567c35c5c8b88442
abela-a <abelardhana96@gmail.com>
2022-03-14 11:53:30 +0800

    eps.9 - creating our homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,19 @@
 <template>
-  <Tutorial/>
+    <div>
+      <div v-for="home in homes" :key="home.ObjectID" style="float:left;margin:10px">
+        <home-card :home="home"/>
+      </div>
+    </div>
 </template>
 
 <script>
+import homes from '~/data/homes';
+
 export default {
-  name: 'IndexPage'
+  data() {
+    return  {
+      homes : homes.slice(0, 3)
+    }
+  }
 }
 </script>
