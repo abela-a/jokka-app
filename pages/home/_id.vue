d3b694eac842bf305f87014d0cbdcc7b998de025
abela-a <abelardhana96@gmail.com>
2022-03-15 13:33:10 +0800

    eps.19 - problem 1 - network latency

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    <div style="display:flex">
-      <img v-for="image in home.images" :src="image" :key="image" width="200" height="150"/>
+    <div style="display:flex;">
+      <img v-for="image in home.images" :key="image" :src="image" width="200" height="150">
     </div>
     {{ home.title }} <br>
     ${{ home.pricePerNight }} / night <br>
-    <img src="/images/marker.svg" width="20" height="20"/> {{ home.location.address }} {{ home.location.city }} {{ home.location.state }} {{ home.location.country }} <br>
-    <img src="/images/star.svg" width="20" height="20"/> {{ home.reviewValue }} <br>
-    {{ home.guests }} guests, {{ home.bedrooms }} bedrooms, {{ home.beds }} beds, {{ home.bathrooms }} bath <br>
+    <img src="/images/marker.svg" width="20" height="20"/>{{ home.location.address }} {{ home.location.city }} {{ home.location.state }} {{ home.location.country }}<br/>
+    <img src="/images/star.svg" width="20" height="20"/>{{ home.reviewValue}} <br/>
+    {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds, {{ home.bathrooms }} bath<br/>
+    {{ home.description }}
     <div style="height:800px;width:800px" ref="map"></div>
   </div>
 </template>
@@ -21,9 +22,13 @@ export default {
       title: this.home.title,
       script: [
         {
-          src: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBD9zeuZiS50WVLOGKXnnL7xcuRsvlCQSg&libraries=places',
+          src:'https://maps.googleapis.com/maps/api/js?key=AIzaSyBD9zeuZiS50WVLOGKXnnL7xcuRsvlCQSg&libraries=places&callback=initMap',
           hid: 'map',
-          defer: true,
+          async: true,
+          skip: process.client && window.mapLoaded
+        }, {
+          innerHTML: 'window.initMap = function(){ window.mapLoaded = true }',
+          hid: 'map-init',
         }
       ]
     }
@@ -33,21 +38,31 @@ export default {
       home: {},
     }
   },
-  mounted() {
-    const mapOptions = {
-      zoom: 18,
-      center: new window.google.maps.LatLng(this.home._geoloc.lat, this.home._geoloc.lang),
-      disableDefaultUI: true,
-      zoomControl: true,
+  methods:{
+    showMap(){
+      const mapOptions = {
+        zoom: 18,
+        center: new window.google.maps.LatLng(this.home._geoloc.lat, this.home._geoloc.lng),
+        disableDefaultUI: true,
+        zoomControl: true,
+      }
+      const map = new window.google.maps.Map(this.$refs.map, mapOptions)
+      const position = new window.google.maps.LatLng(this.home._geoloc.lat, this.home._geoloc.lng)
+      const marker = new window.google.maps.Marker({ position })
+      marker.setMap(map)
     }
-    const map = new window.google.maps.Maps(this.$ref.map, mapOptions)
-    const position = new window.google.maps.LatLng(this.home._geoloc.lat, this.home._geoloc.lang)
-    const marker = new window.google.maps.Marker({ position })
-    marker.setMap(map)
   },
-  created() {
+  mounted(){
+    const timer = setInterval(() => {
+      if(window.mapLoaded){
+        clearInterval(timer)
+        this.showMap()
+      }
+    }, 200)
+  },
+  created(){
     const home = homes.find((home) => home.objectID == this.$route.params.id)
     this.home = home
-  },
-};
+  }
+}
 </script>
